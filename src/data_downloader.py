568e97a4ba15121c59233fd47c6136af39b050ad
WhiteCmile <whitecmile@WhiteCmile>
2025-01-16 15:00:13 +0800

    feat(src): update main.py; add data_downloader

---
+++ src/data_downloader.py
@@ -0,0 +1,61 @@
+import zstandard as zstd
+import shutil
+import subprocess
+import sys
+import os
+from tqdm import tqdm
+
+def download_data_from_url(dataset_url, dataset_path):
+    """
+    Download data from a URL and save it to a specified path
+    """
+    try:
+        result = subprocess.run([
+            "wget",
+            "-r",
+            "-l1",
+            "--no-parent",
+            "-A",
+            ".zst",
+            "-nd",
+            "-P",
+            dataset_path,
+            dataset_url,
+        ], check = True) 
+    except: 
+        print("Error downloading the data")
+        sys.exit(1)
+
+def decompress_zst(dataset_path):
+    """
+    Decompress all .zst files in a directory
+        @param dataset_path: path to the directory containing the .zst files
+        @return: a list of the decompressed files
+    """
+    with os.scandir(dataset_path) as entries:
+        data_list = []
+        os.chdir(dataset_path)
+        for entry in tqdm(entries, desc = "Decompressing .zst files"):
+            if entry.is_file() and entry.name.endswith(".zst"):
+                with open(entry, 'rb') as compressed_file:
+                    # Do not decompress if the file is already decompressed
+                    if entry.name.rstrip(".zst") not in os.listdir(dataset_path):
+                        with open(entry.name.rstrip('.zst'), 'wb') as destination:
+                            dctx = zstd.ZstdDecompressor()
+                            with dctx.stream_reader(compressed_file) as reader:
+                                shutil.copyfileobj(reader, destination)
+                                data_list.append(os.path.join(dataset_path, entry.name.rstrip('.zst')))
+    
+    return data_list
+
+def download_data(args):
+    dataset_url = args["dataset_url"]
+    dataset_path = args["dataset_path"]
+
+    if not os.path.exists(dataset_path):
+        os.makedirs(dataset_path)
+        download_data_from_url(dataset_url, dataset_path)
+
+    data_list = decompress_zst(dataset_path)
+
+    return data_list
---
+++ src/main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import argparse
+import data_downloader
+
+def read_config(config_file):
+    with open(config_file, 'r') as stream:
+        try:
+            import yaml
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            exit(1)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", type = str, default = "../config/config.yaml")
+    args = parser.parse_args()
+    args = read_config(args.config_file)
+
+    traces_list = data_downloader.download_data(args)
+
+    print (args)
+
+if __name__ == "__main__":
+    main()

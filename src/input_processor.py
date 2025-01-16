0b5ae9a1437816bf9d7b57a341e9258ca61807b2
WhiteCmile <whitecmile@WhiteCmile>
2025-01-16 15:23:12 +0800

    feat(src): add input_processor for processing the trace files

---
+++ src/input_processor.py
@@ -0,0 +1,43 @@
+import sys
+import struct
+from typing import NamedTuple, List
+
+class Request(NamedTuple):
+    timestamp: int
+    obj_id: int
+    obj_size: int
+    next_access_vtime: int
+
+class InputProcessor:
+    def process_input(self, trace_file):
+        requests = self._parse_trace(trace_file)
+        return requests
+
+    def _parse_trace(self, trace_file):
+        # format string for unpacking binary data
+        # '<' means little-endian
+        # 'I': uint32_t, 4 bytes
+        # 'Q': uint64_t, 8 bytes
+        # 'q': int64_t, 8 bytes
+        format_str = '<IQIq'
+    
+        record_size = struct.calcsize(format_str)
+        print ("record_size: ", record_size)
+    
+        requests = []
+    
+        with open(trace_file, 'rb') as f:
+            while True:
+                chunk = f.read(record_size)
+                if len(chunk) != record_size:
+                    if len(chunk) == 0:
+                        break
+                    print (f"Invalid chunk size: {len(chunk)} in trace {trace_file}", file = sys.stderr)
+                    exit(1)
+            
+                unpacked_data = struct.unpack(format_str, chunk)
+            
+                request = Request(*unpacked_data)
+                requests.append(request)
+    
+        return requests

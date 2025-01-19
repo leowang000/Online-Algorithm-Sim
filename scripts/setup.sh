4acb4390dc76787823e61f56a392e4954571bfd6
WhiteCmile <whitecmile@WhiteCmile>
2025-01-19 16:30:34 +0800

    feat(., scripts): rename the compile.sh; refine the project layout

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,14 @@
 
 # Shell script to compile the required .so files for the python code
 
+# Install the required packages
+
+pip install pybind11
+pip install zstandard
+pip install tqdm
+
+# Run this script from the root directory of the project
+
 cd src 
 python3 setup.py build_ext --inplace 
 cd scheduler
